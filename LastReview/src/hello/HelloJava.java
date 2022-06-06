41faf3360e6d687e399c7cf4d64ac9af6834bc44
ygh65 <ygh65@192.168.8.74>
2022-06-06 17:17:35 +0900

    git hub 테스트 중.. 메시지 추가

--- LastReview/src/hello/HelloJava.java
+++ LastReview/src/hello/HelloJava.java
@@ -4,6 +4,7 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello, Java");
 		System.out.println("My name is 가현 ^^");
+		System.out.println("git hub 테스트 중임 제발 제발!! ");
 	}
 
 }
